805e640c176c9e8eae00a0e145c259cb8f50151d
MRxMIRROR <127676677+MRxMIRROR@users.noreply.github.com>
2026-07-09 16:15:07 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("API_ID", "14619078"))
+API_HASH = os.getenv("API_HASH", "c90576cc470b4bd4dc08396cfa449833")
 
 # Source channels processing
 source_channels_raw = os.getenv("SOURCE_CHANNELS", "")
